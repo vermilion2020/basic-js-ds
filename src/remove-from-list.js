e5084176eaa65ed63833fe3b12a09bd50fadbbde
Mila <99rest9900@gmail.com>
2022-04-29 07:11:58 +0300

    feat: add remove-from-list draft

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -14,17 +14,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For l = [3, 1, 2, 3, 4, 5] and k = 3,
  * the output should be [1, 2, 4, 5]
  *
- * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
- */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  Singly - linked lists are already defined using interface*/
+
+  class ListNode {
+    constructor(x) {
+      this.value = x;
+      this.next = null;
+    }
+  }
+ 
+function removeKFromList( l, k ) {
+  let current = l;
+  let prev;
+
+  while (current.next !== null) {
+    if (current.value === k && current.next.value === k) {
+      while (current.next.value === k) {
+        prev = current;
+        current = current.next;
+      }
+       current.value = current.next.value;
+       current.next = current.next.next;
+    }
+    prev = current;
+    current = current.next;
+    console.log(current.value)
+  }
+
+  (current.next === null && current.value === k) ? prev.next = null : '';
+
+  return l;
 }
 
 module.exports = {
